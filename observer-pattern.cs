b12ba3efd07c1811c14b34201e3d82b76b22a02f
Muhammad Sufyan Patel <69216156+msufiyanpatel@users.noreply.github.com>
2023-08-08 17:36:50 +0500

    2 more patterns edit

---
+++ observer-pattern.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Observer Pattern Example \n \n");
+
+Player player = new Player();
+HealthDisplay healthDisplay = new HealthDisplay();
+SpecialEffect specialEffect = new SpecialEffect();
+
+player.Attach(healthDisplay);
+player.Attach(specialEffect);
+
+player.Health = 40;
+
+player.Detach(healthDisplay);
+
+player.Health = 10;
+
+Console.ReadKey();
+
+interface IObserver
+{
+    void onHealthChanged(int newHealth);
+}
+
+interface ISubject
+{
+    void Attach(IObserver observer);
+    void Detach(IObserver observer);
+    void Notify(int newHealth);
+}
+
+class Player : ISubject
+{
+    private int health;
+
+    private List<IObserver> observers= new List<IObserver>();
+
+    public int Health
+    {
+        get { return health; }
+        set
+        {
+            if(health != value)
+            {
+                health = value;
+                Notify(health);
+            }
+        }
+    }
+
+    public void Attach(IObserver observer)
+    {
+        observers.Add(observer);
+    }
+
+    public void Detach(IObserver observer)
+    {
+        observers.Remove(observer);
+    }
+
+    public void Notify(int newHealth)
+    {
+        foreach(var observer in observers)
+        {
+            observer.onHealthChanged(newHealth);
+        }
+    }
+}
+
+
+class HealthDisplay : IObserver
+{
+    public void onHealthChanged(int newHealth)
+    {
+        Console.WriteLine($"Health: {newHealth}");
+    }
+
+
+}
+
+class SpecialEffect : IObserver
+{
+    public void onHealthChanged(int newHealth)
+    {
+        if(newHealth < 30)
+        Console.WriteLine($"Low Health: {newHealth}");
+    }
+
+
+}
+
---
+++ strategy-pattern.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Strategy Pattern Example");
+
+PaymentContext paymentContext = new PaymentContext(new CreditCardPayment());
+
+paymentContext.ProcessPayment(100.0);
+
+paymentContext.SetPaymentStrategy(new PayPalPayment());
+paymentContext.ProcessPayment(50.0);
+
+paymentContext.SetPaymentStrategy(new BitcoinPayment());
+paymentContext.ProcessPayment(200.0);
+
+Console.ReadKey();
+
+
+public interface IPaymentStrategy
+{
+    void ProcessPayment(double amount);
+}
+
+public class CreditCardPayment : IPaymentStrategy
+{
+    public void ProcessPayment(double amount)
+    {
+        
+        Console.WriteLine($"Processing credit card payment of {amount} USD.");
+    }
+}
+
+public class PayPalPayment : IPaymentStrategy
+{
+    public void ProcessPayment(double amount)
+    {
+        
+        Console.WriteLine($"Processing PayPal payment of {amount} USD.");
+    }
+}
+
+public class BitcoinPayment : IPaymentStrategy
+{
+    public void ProcessPayment(double amount)
+    {
+        
+        Console.WriteLine($"Processing Bitcoin payment of {amount} USD.");
+    }
+}
+
+public class PaymentContext
+{
+    private IPaymentStrategy paymentStrategy;
+
+    public PaymentContext(IPaymentStrategy paymentStrategy)
+    {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void SetPaymentStrategy(IPaymentStrategy paymentStrategy)
+    {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void ProcessPayment(double amount)
+    {
+        paymentStrategy.ProcessPayment(amount);
+    }
+}
+
+
